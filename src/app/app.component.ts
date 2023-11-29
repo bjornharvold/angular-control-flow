afb3eeccf08951bce63ffdfe8956f6859aa00270
Bjorn Harvold <bjorn@harvold.com>
2023-11-29 16:31:43 +0700

    build: simple control flow formatting issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,7 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
-import {
-  DifferenceInSecondsPipeModule,
-  DifferenceInMinutesPipeModule,
-  DifferenceInHoursPipeModule,
-  DifferenceInDaysPipeModule,
-  DifferenceInWeeksPipeModule,
-  DifferenceInMonthsPipeModule,
-  DifferenceInYearsPipeModule,
-  ParseIsoPipeModule,
-  IsEqualPipeModule,
-} from 'ngx-date-fns';
 import { NgIf } from '@angular/common';
-import { formatISO } from 'date-fns';
-
-const SECONDS_IN_YEAR = 31536000;
-const SECONDS_IN_MONTH = 2628288;
-const SECONDS_IN_WEEK = 604800;
-const SECONDS_IN_DAY = 86400;
-const SECONDS_IN_HOUR = 3600;
-const SECONDS_IN_MINUTE = 60;
-const DAYS_IN_YEAR = 365;
-const DAYS_IN_MONTH = 30;
-const DAYS_IN_WEEK = 7;
 
 @Component({
   selector: 'app-root',
@@ -40,30 +18,10 @@ const DAYS_IN_WEEK = 7;
   standalone: true,
   imports: [
     NgIf,
-    DifferenceInSecondsPipeModule,
-    DifferenceInMinutesPipeModule,
-    DifferenceInHoursPipeModule,
-    DifferenceInDaysPipeModule,
-    DifferenceInWeeksPipeModule,
-    DifferenceInMonthsPipeModule,
-    DifferenceInYearsPipeModule,
-    ParseIsoPipeModule,
-    IsEqualPipeModule,
   ],
 })
 export class AppComponent implements OnInit {
-  @Input() isoDateTime?: string = formatISO(new Date(2019, 8, 18, 19, 0, 52));
-  @Input() isoDate?: string;
-  @Input() now = new Date();
-  secondsInYear = SECONDS_IN_YEAR;
-  secondsInMonth = SECONDS_IN_MONTH;
-  secondsInWeek = SECONDS_IN_WEEK;
-  secondsInDay = SECONDS_IN_DAY;
-  secondsInHour = SECONDS_IN_HOUR;
-  secondsInMinute = SECONDS_IN_MINUTE;
-  daysInYear = DAYS_IN_YEAR;
-  daysInMonth = DAYS_IN_MONTH;
-  daysInWeek = DAYS_IN_WEEK;
+  @Input() view: any;
 
   constructor() {}
 
